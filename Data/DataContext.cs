9447380620e0d7a481e7ecdd567cab5f31ad3bf8
sudhir <sudhirvetri@gmail.com>
2022-12-21 01:27:22 +0530

    Initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; } // users here represent the name of the table in db
+    }
+}
